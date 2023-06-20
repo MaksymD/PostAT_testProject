2c3802e8e43dee39f590597e285048e8d4ff52da
maksym.dotsenko <maksym.dotsenko@ebcont.com>
2023-06-20 18:33:07 +0200

    add: negative tests

---
+++ PostATAutomationProject/Readme.md
@@ -0,0 +1,25 @@
+﻿### TODO:
+
+***- add property file***
+
+***- improve reporting***
+
+### LOGIN DATA:
+- EMAIL-ADRESSE: max.musterman.postat.test@gmail.com
+- PASSWORT: postATtest2023!
+- KUNDENNUMMER: 5295007
+
+### MORE POSSIBLE TESTS:
+
+**Positive Tests (Valid Credentials):**
+
+> Successful login after resetting the password in "Login & Einstellungen" with a new password.
+> Successful login after resetting the password in Login Page by clicking "Passwort vergessen?" and resetting to a new password.
+
+**Negative Tests (Invalid Credentials):**
+
+>Login attempt with a locked or disabled user account.
+
+>Login attempt with an expired password (if it possible).
+
+>Login attempt with SQL injection and similar security testing in the username/email and password fields.
--- PostATAutomationProject/Tests/LoginTest.cs
+++ PostATAutomationProject/Tests/LoginTest.cs
@@ -71,7 +71,7 @@ public class LoginTest : BaseTest
 
     [Test, Order(2)]
     [Category("Selenium")]
-    public void TestNegative_LoginWithoutCredentials()
+    public void TestNegative_LoginWithEmptyCredentials()
     {
         using (var driver = InitializeDriver())
         {
@@ -105,6 +105,186 @@ public class LoginTest : BaseTest
                 Assert.True(driver.FindElement(By.XPath("//p[contains(@role, 'alert') and contains(text(), 'Bitte E-Mail Adresse eingeben.')]")).Displayed);
                 Assert.True(driver.FindElement(By.XPath("//p[contains(@role, 'alert') and contains(text(), 'Bitte Passwort eingeben.')]")).Displayed);
                 log.Info("Email/Password errors are displayed.");
+            }
+            finally
+            {
+                commonUtils.PrintLogs("browser", driver);
+            }
+        }
+    }
+    
+    [Test, Order(3)]
+    [Category("Selenium")]
+    public void TestNegative_LoginInvalidEmail()
+    {
+        using (var driver = InitializeDriver())
+        {
+            try
+            {
+                // navigate to "post.at" Page and check title
+                StartPage startPage = new StartPage(driver);
+                Assert.That("Post AG - PostAG", Is.EqualTo(startPage.GetPageTitle()));
+                log.Info("Post AG start Page opened.");
+                
+                // click accept cookies button
+                bool elementCookiesExists = driver.FindElements(By.XPath("//button[@id = 'onetrust-accept-btn-handler']")).Count > 0;
+                if (elementCookiesExists)
+                {
+                    startPage.AcceptCookie();
+                    log.Info("Cookies accepted.");
+                }
+                
+                // click login button on top-menu
+                startPage.selectLoginInHeaderbar();
+                log.Info("'Einloggen / Registrieren' button clicked.");
+                
+                // navigate to "login.post.at" Page and check title
+                UserLoginPage userLoginPage = new UserLoginPage(driver);
+                Assert.That("Userlogin - Default", Is.EqualTo(userLoginPage.GetPageTitle()));
+                log.Info("User Login Post AG Page opened.");
+
+                // click 'Jetzt einloggen' button and check if Login failed message is displayed 
+                userLoginPage.inputLoginCredentials("test", "postATtest2023!");
+                userLoginPage.clickLoginButton();
+                userLoginPage.clickLoginButton();
+                Thread.Sleep(1000);
+                Assert.True(driver.FindElement(By.XPath("//p[contains(@role, 'alert') and contains(text(), 'Ungültige E-Mail Adresse.')]")).Displayed);
+                log.Info("Login failed message is displayed.");
+            }
+            finally
+            {
+                commonUtils.PrintLogs("browser", driver);
+            }
+        }
+    }
+    
+    [Test, Order(4)]
+    [Category("Selenium")]
+    public void TestNegative_LoginWrongEmail()
+    {
+        using (var driver = InitializeDriver())
+        {
+            try
+            {
+                // navigate to "post.at" Page and check title
+                StartPage startPage = new StartPage(driver);
+                Assert.That("Post AG - PostAG", Is.EqualTo(startPage.GetPageTitle()));
+                log.Info("Post AG start Page opened.");
+                
+                // click accept cookies button
+                bool elementCookiesExists = driver.FindElements(By.XPath("//button[@id = 'onetrust-accept-btn-handler']")).Count > 0;
+                if (elementCookiesExists)
+                {
+                    startPage.AcceptCookie();
+                    log.Info("Cookies accepted.");
+                }
+                
+                // click login button on top-menu
+                startPage.selectLoginInHeaderbar();
+                log.Info("'Einloggen / Registrieren' button clicked.");
+                
+                // navigate to "login.post.at" Page and check title
+                UserLoginPage userLoginPage = new UserLoginPage(driver);
+                Assert.That("Userlogin - Default", Is.EqualTo(userLoginPage.GetPageTitle()));
+                log.Info("User Login Post AG Page opened.");
+
+                // click 'Jetzt einloggen' button and check if Login failed message is displayed 
+                userLoginPage.inputLoginCredentials("test@gmail.com", "postATtest2023!");
+                userLoginPage.clickLoginButton();
+                Thread.Sleep(1000);
+                Assert.True(driver.FindElement(By.XPath("//h2[contains(text(), 'Login fehlgeschlagen')]")).Displayed);
+                Assert.True(driver.FindElement(By.XPath("//h3[contains(text(), 'Bitte versuchen Sie es nochmals!')]")).Displayed);
+                log.Info("Login failed message is displayed.");
+            }
+            finally
+            {
+                commonUtils.PrintLogs("browser", driver);
+            }
+        }
+    }
+    
+    [Test, Order(5)]
+    [Category("Selenium")]
+    public void TestNegative_LoginWrongPassword()
+    {
+        using (var driver = InitializeDriver())
+        {
+            try
+            {
+                // navigate to "post.at" Page and check title
+                StartPage startPage = new StartPage(driver);
+                Assert.That("Post AG - PostAG", Is.EqualTo(startPage.GetPageTitle()));
+                log.Info("Post AG start Page opened.");
+                
+                // click accept cookies button
+                bool elementCookiesExists = driver.FindElements(By.XPath("//button[@id = 'onetrust-accept-btn-handler']")).Count > 0;
+                if (elementCookiesExists)
+                {
+                    startPage.AcceptCookie();
+                    log.Info("Cookies accepted.");
+                }
+                
+                // click login button on top-menu
+                startPage.selectLoginInHeaderbar();
+                log.Info("'Einloggen / Registrieren' button clicked.");
+                
+                // navigate to "login.post.at" Page and check title
+                UserLoginPage userLoginPage = new UserLoginPage(driver);
+                Assert.That("Userlogin - Default", Is.EqualTo(userLoginPage.GetPageTitle()));
+                log.Info("User Login Post AG Page opened.");
+
+                // click 'Jetzt einloggen' button and check if Login failed message is displayed 
+                userLoginPage.inputLoginCredentials("max.musterman.postat.test@gmail.com", "test");
+                userLoginPage.clickLoginButton();
+                Thread.Sleep(1000);
+                Assert.True(driver.FindElement(By.XPath("//h2[contains(text(), 'Login fehlgeschlagen')]")).Displayed);
+                Assert.True(driver.FindElement(By.XPath("//h3[contains(text(), 'Bitte versuchen Sie es nochmals!')]")).Displayed);
+                log.Info("Login failed message is displayed.");
+            }
+            finally
+            {
+                commonUtils.PrintLogs("browser", driver);
+            }
+        }
+    }
+    
+    [Test, Order(6)]
+    [Category("Selenium")]
+    public void TestNegative_LoginWrongUsernameAndPassword()
+    {
+        using (var driver = InitializeDriver())
+        {
+            try
+            {
+                // navigate to "post.at" Page and check title
+                StartPage startPage = new StartPage(driver);
+                Assert.That("Post AG - PostAG", Is.EqualTo(startPage.GetPageTitle()));
+                log.Info("Post AG start Page opened.");
+                
+                // click accept cookies button
+                bool elementCookiesExists = driver.FindElements(By.XPath("//button[@id = 'onetrust-accept-btn-handler']")).Count > 0;
+                if (elementCookiesExists)
+                {
+                    startPage.AcceptCookie();
+                    log.Info("Cookies accepted.");
+                }
+                
+                // click login button on top-menu
+                startPage.selectLoginInHeaderbar();
+                log.Info("'Einloggen / Registrieren' button clicked.");
+                
+                // navigate to "login.post.at" Page and check title
+                UserLoginPage userLoginPage = new UserLoginPage(driver);
+                Assert.That("Userlogin - Default", Is.EqualTo(userLoginPage.GetPageTitle()));
+                log.Info("User Login Post AG Page opened.");
+
+                // click 'Jetzt einloggen' button and check if Login failed message is displayed
+                userLoginPage.inputLoginCredentials("test@gmail.com", "test");
+                userLoginPage.clickLoginButton();
+                Thread.Sleep(1000);
+                Assert.True(driver.FindElement(By.XPath("//h2[contains(text(), 'Login fehlgeschlagen')]")).Displayed);
+                Assert.True(driver.FindElement(By.XPath("//h3[contains(text(), 'Bitte versuchen Sie es nochmals!')]")).Displayed);
+                log.Info("Login failed message is displayed.");
             }
             finally
             {
